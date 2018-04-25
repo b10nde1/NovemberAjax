48436157dfdcc837412ed9f69d4431923901db7f
Ricky Blondel <rickyblondel@icloud.com>
2018-04-25 18:17:01 +0400

    Init Project
    init project, 1st commit and push

---
+++ NovemberAjax/popup.js
@@ -0,0 +1,54 @@
+let htmlFile=document.getElementById('htmlFile');
+
+htmlFile.onchange=function(activeTab){
+    var input = event.target;
+    var reader = new FileReader();
+    var searchKey=document.getElementById('searchKey').value;
+    reader.onload = function(){
+        var dataURL = reader.result;
+        document.getElementById("code-to-scan").innerHTML=dataURL;
+        scanDocument(searchKey);
+    };
+    var result = reader.readAsText(input.files[0]);
+}
+
+const scanDocument=(arg)=> {
+    var totalElements=document.getElementsByClassName(arg).length;
+    var arrayElements="";
+    for(var compteur=0;compteur<totalElements;compteur++){
+      var urlFromHtml=document.getElementsByClassName(arg)[compteur].getAttribute('href');
+      if(urlFromHtml!='javascript:void(0)'){
+          arrayElements=arrayElements+"<p>"+urlFromHtml+"</p>";
+          openPage(urlFromHtml);
+      }
+    }
+    document.getElementById("total-count").innerHTML=totalElements-1;
+    document.getElementById("list-urls").innerHTML=arrayElements;
+    copyElement(document.getElementById("list-urls"));
+  }
+
+const openPage=(urlIn)=>{
+    chrome.tabs.create({ url: urlIn , selected: false});
+    console.log("url || "+urlIn+" || ok");
+}
+
+const copyElement=(el)=>{
+    var body = document.body, range, sel;
+    if (document.createRange && window.getSelection) {
+        range = document.createRange();
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        try {
+            range.selectNodeContents(el);
+            sel.addRange(range);
+        } catch (e) {
+            range.selectNode(el);
+            sel.addRange(range);
+        }
+    } else if (body.createTextRange) {
+        range = body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
+    }
+    document.execCommand("Copy");
+}
